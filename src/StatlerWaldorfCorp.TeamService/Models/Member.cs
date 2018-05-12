3993b9690927f160d9a8101b74e1fb3da146bd7c
Daniel Kamau <jasmine@Daniels-iMac.local>
2018-05-12 18:38:51 +0400

    Initial Commit

---
+++ src/StatlerWaldorfCorp.TeamService/Models/Member.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StatlerWaldorfCorp.TeamService.Models
+{ 
+    public class Member 
+    { 
+        #region 
+        public Member() { } 
+        public Member(Guid id) : this() { this.ID = id; } 
+        public Member(string firstName, string lastName, Guid id) : this(id) 
+        { 
+            this.FirstName = firstName;
+            this.LastName = lastName; 
+        } 
+        #endregion
+
+        #region Members
+        public Guid ID { get; set; } 
+        public string FirstName { get; set; } 
+        public string LastName { get; set; } 
+        #endregion
+
+        #region Behaviour
+        public override string ToString() { return this.LastName; } 
+        #endregion
+    }
+}
---
+++ src/StatlerWaldorfCorp.TeamService/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StatlerWaldorfCorp.TeamService.Models
+{ 
+    public class Team 
+    { 
+        #region Constructors
+        public Team(string name) : this() { this.Name = name; } 
+        public Team(string name, Guid id) : this(name) { this.ID = id; } 
+        public Team() { this.Members = new List<Member>(); } 
+        #endregion
+
+        #region Members
+        public string Name { get; set; } 
+        public Guid ID { get; set; } 
+        public ICollection<Member> Members { get; set; } 
+        #endregion
+        
+        #region Behaviours
+        public override string ToString() { return this.Name; } }
+        #endregion
+}
